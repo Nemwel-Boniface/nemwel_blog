10bdd05c3d9b617ee8949fe712518a755e1e024b
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-06 22:37:56 +0300

    Update method which returns the 5 most recent comments for a given post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments
   has_many :likes
+
+  def most_recent_posts(id)
+    Comment.order(created_at: :desc).where(author_id: id).limit(5)
+  end
 end
