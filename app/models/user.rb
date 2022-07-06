0c7cea5fb743fe5c280669267d6721f76bc37b31
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-06 22:16:14 +0300

    Update added method that returns 3 posts by a certain user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
-  has_many :likes, foreign_key: :
+  has_many :likes, foreign_key: :author_id
+
+  def most_recent_posts(id)
+    Post.order(created_at: :desc).where(author_id: id).limit(3)
+  end
 end
