ef5373593524f08b04b4f386d475f1c23456d765
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-07 07:58:34 +0300

    Update created method to update the posts counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  def update_posts_count
+    author.update(post_counter: author.posts.count)
+  end
+
   def most_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
