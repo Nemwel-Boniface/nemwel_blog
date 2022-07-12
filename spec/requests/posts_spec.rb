b3b220062352c6d6e6d37c146d9d428ffc3bb77f
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-12 22:45:03 +0300

    Update included tests for Posts show

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -13,6 +13,19 @@ RSpec.describe 'Posts', type: :request do
       it 'renders correct contents' do
       expect(response.body).to include('Welcome to my Posts page!')
       end
+    end
+
+    describe 'GET /show' do
+      before(:example) { get '/users/1/posts' }
+        it 'responds with the code 400' do
+          expect(response).to have_http_status(:success)
+        end
+        it "renders 'show' view" do
+          expect(response).to render_template(locals: 'posts/show')
+        end
+        it 'renders contents in the view' do
+          expect(response.body).to include('Welcome to my Posts page!')
+        end
       end
   end
 end
