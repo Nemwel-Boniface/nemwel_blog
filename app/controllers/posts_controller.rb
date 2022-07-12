a47834a04515ef465fe727dfb6ded2da233287a2
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-12 12:52:29 +0300

    Update to the Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,8 @@
-class PostsController < ApplicationController
+class UserController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+    def show
+        @posts = Post.find(params[:id])
+    end
 end
