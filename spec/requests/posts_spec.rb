c786f1a2296291564fd849864dd6f7cbef24d353
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-12 22:45:57 +0300

    Update checked and fixed all found offences

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -5,27 +5,27 @@ RSpec.describe 'Posts', type: :request do
     describe 'GET /index' do
       before(:example) { get '/users/1/posts' }
       it 'responds with the code 200' do
-      expect(response).to have_http_status(:ok)
+        expect(response).to have_http_status(:ok)
       end
       it "renders 'index' template" do
-      expect(response).to render_template(:index)
+        expect(response).to render_template(:index)
       end
       it 'renders correct contents' do
-      expect(response.body).to include('Welcome to my Posts page!')
+        expect(response.body).to include('Welcome to my Posts page!')
       end
     end
 
     describe 'GET /show' do
       before(:example) { get '/users/1/posts' }
-        it 'responds with the code 400' do
-          expect(response).to have_http_status(:success)
-        end
-        it "renders 'show' view" do
-          expect(response).to render_template(locals: 'posts/show')
-        end
-        it 'renders contents in the view' do
-          expect(response.body).to include('Welcome to my Posts page!')
-        end
+      it 'responds with the code 400' do
+        expect(response).to have_http_status(:success)
       end
+      it "renders 'show' view" do
+        expect(response).to render_template(locals: 'posts/show')
+      end
+      it 'renders contents in the view' do
+        expect(response.body).to include('Welcome to my Posts page!')
+      end
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,27 +3,27 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     before(:example) { get '/users' }
-      it 'returns http success' do
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
-      it 'renders users index view' do
+    it 'renders users index view' do
       expect(response).to render_template(:index)
     end
-      it 'shows content in the view' do
+    it 'shows content in the view' do
       expect(response.body).to include('Welcome to my users page!')
     end
-  end 
+  end
 
   describe 'GET /show' do
     before(:example) { get users_path }
-      it 'responds with the code 400' do
-        expect(response).to have_http_status(:success)
-      end
-      it 'renders show view' do
-        expect(response).to render_template(locals: 'users/show')
-      end
-      it 'shows contents in the view' do
-        expect(response.body).to include('Welcome to my users page!')
-      end
-    end 
+    it 'responds with the code 400' do
+      expect(response).to have_http_status(:success)
+    end
+    it 'renders show view' do
+      expect(response).to render_template(locals: 'users/show')
+    end
+    it 'shows contents in the view' do
+      expect(response.body).to include('Welcome to my users page!')
+    end
+  end
 end
