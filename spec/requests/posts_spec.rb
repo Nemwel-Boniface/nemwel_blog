c7d3cf1134db78ad20c82f64c9afbc7a70df54c1
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-12 22:40:36 +0300

    Update included tests for Posts index

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    describe 'GET /index' do
+      before(:example) { get '/users/1/posts' }
+      it 'responds with the code 200' do
+      expect(response).to have_http_status(:ok)
+      end
+      it "renders 'index' template" do
+      expect(response).to render_template(:index)
+      end
+      it 'renders correct contents' do
+      expect(response.body).to include('Welcome to my Posts page!')
+      end
+      end
   end
 end
