0463bbeedf58b85c2d8f487af77aa731374bb93e
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-19 16:01:45 +0300

    Upate to project files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post, class_name: 'Post', foreign_key: :post_id
 
-  after_save :update_comments_count
-
   def update_comments_count
     post.increment!(:comments_counter)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_082432) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_19_123127) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,10 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_14_082432) do
     t.datetime "updated_at", null: false
     t.bigint "post_id"
     t.bigint "author_id"
-    t.bigint "user_id", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -58,7 +56,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_14_082432) do
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
   add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
